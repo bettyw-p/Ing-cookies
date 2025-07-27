196d14c83003ac8d376c22a3b011fa9a1a3b9e7f
bettyw-p <b.wozniak.pasierb@gmail.com>
2025-07-27 08:46:41 +0200

    Dismiss cookies test added and code refactor

--- Tests/test_cookies.py
+++ Tests/test_cookies.py
@@ -1,30 +1,29 @@
 from playwright.sync_api import Page, expect
+import pytest
 from pages.cookie_banner import CookieBanner
 from config import BASE_URL
 
+@pytest.fixture(autouse=True)
+def go_to_base_url(page):
+    page.goto(BASE_URL)
+
 def test_accept_cookies(page: Page):
     cookie_banner = CookieBanner(page)
-    page.goto(BASE_URL)
     cookie_banner.customize_button.click()
-
-    state = cookie_banner.get_toggle_state(cookie_banner.analytical_toggle)
-
-    if state == "false":
-        cookie_banner.analytical_toggle.click()
-    else:
-        pass
-
-    state = cookie_banner.get_toggle_state(cookie_banner.analytical_toggle)
-
+    cookie_banner.analytical_toggle.click()
     expect(cookie_banner.analytical_toggle).to_be_checked()
 
     cookie_banner.accept_selected.click()
-
     expect(page).to_have_title(cookie_banner.page_title)
     expect(cookie_banner.accept_selected).to_be_hidden()
 
-    cookies = page.context.cookies()
-    cookie_dict = {cookie['name']: cookie['value'] for cookie in cookies}
-    expected_value = '3'
-    actual_value = cookie_dict.get('cookiePolicyGDPR')
-    assert actual_value == expected_value
+    cookie_banner.check_accepted_cookies(page, "3")
+
+def test_dismiss_cookies(page: Page):
+    cookie_banner = CookieBanner(page)
+    cookie_banner.dismiss_all.click()
+
+    expect(page).to_have_title(cookie_banner.page_title)
+    expect(cookie_banner.dismiss_all).to_be_hidden()
+
+    cookie_banner.check_accepted_cookies(page, "1")
--- pages/cookie_banner.py
+++ pages/cookie_banner.py
@@ -6,7 +6,15 @@ class CookieBanner:
         self.page_title = "ING Bank Śląski"
         self.customize_button = page.locator("//button[contains(text(), 'Dostosuj')]")
         self.accept_selected = page.locator("//button[contains(text(), 'Zaakceptuj zaznaczone')]")
+        self.dismiss_all = page.locator("//button[contains(@class,'js-cookie-policy-main-decline-button')]")
         self.analytical_toggle = page.locator('[role="switch"][name="CpmAnalyticalOption"]')
 
     def get_toggle_state(self, toggle_type):
         return toggle_type.get_attribute("aria-checked")
+    
+    def check_accepted_cookies(self, page: Page, expected_value):
+        cookies = page.context.cookies()
+        cookie_dict = {cookie['name']: cookie['value'] for cookie in cookies}
+        expected_value = expected_value
+        actual_value = cookie_dict.get('cookiePolicyGDPR')
+        assert actual_value == expected_value
